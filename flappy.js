58ac07d80e2337d6063594b0b079ff353d452f45
acowden90 <acowden90@gmail.com>
2014-11-01 17:21:34 +0000

    V1.1
    End of Workshop

--- flappy.js
+++ flappy.js
@@ -5,7 +5,10 @@ var stateActions = { preload: preload, create: create, update: update };
 var game = new Phaser.Game(800, 400, Phaser.AUTO, 'game', stateActions);
 var scale;
 var score = 0;
-var player;
+var pipes;
+
+var pipe_offset = 900;
+var pipe_size = 50;
 /*
  * Loads all resources for the game and gives them names.
  */
@@ -14,6 +17,7 @@ function preload() {
     game.load.image("playerImg2", "assets/flappy_batman.png");
     game.load.image("playerImg3", "assets/Ike_2.png");
     game.load.audio("score", "assets/point.ogg");
+    game.load.image("block", "assets/pipe.png");
 }
 
 /*
@@ -21,40 +25,42 @@ function preload() {
  */
 function create() {
     game.stage.setBackgroundColor("#90CCFF");
-    game.add.text (250, 200, "Welcome to my game", {font: "30px Arial", fill: "#ffffff"});
-
+    player = game.add.sprite(100, 200, "playerImg2");
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.physics.arcade.enable(player);
+    player.anchor.setTo(0.5, 0.5);
+    player.scale.setTo(0.6, 0.6);
+    player.checkWorldBounds = true;
+    /*player.body.velocity.x = 100;*/
+    player.body.gravity.y = 200
     game.add.audio("score");
-    game.input.onDown.add(clickHandler);
 
-    game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(spaceHandler);
-    game.input.keyboard.addKey(Phaser.Keyboard.S).onDown.add(sHandler);
-    var x = 100;
-    var y = 200;
-    var rightKey = game.input
+    var spacebar = game.input
         .keyboard
-        .addKey(Phaser.Keyboard.RIGHT);
+        .addKey(Phaser.Keyboard.SPACEBAR);
+    spacebar.onDown.add(player_jump);
 
-    rightKey.onDown.add(moveright);
+    pipes = game.add.group();
 
-    var leftKey = game.input
-        .keyboard
-        .addKey(Phaser.Keyboard.LEFT);
+    game.time.events.loop(1.5*Phaser.Timer.SECOND, generate_pipes);
+}
 
-    leftKey.onDown.add(moveleft);
 
-    var upKey = game.input
-        .keyboard
-        .addKey(Phaser.Keyboard.UP);
+function add_pipe_part (x, y, pipe_part) {
+    var pipe = pipes.create(x, y, pipe_part);
+    game.physics.arcade.enable(pipes);
+    pipe.body.velocity.x = -200;
+}
 
-    upKey.onDown.add(moveup);
-
-    var downKey = game.input
-        .keyboard
-        .addKey(Phaser.Keyboard.DOWN);
-
-    downKey.onDown.add(movedown);
-
-    player = game.add.sprite(x, y, "playerImg2");
+function generate_pipes() {
+    var hole = Math.floor(Math.random()*4)+1;
+    var i;
+    for (i = 0; i < hole; i++) {
+            add_pipe_part (pipe_offset, i * pipe_size, "block");
+    }
+    for (i = hole + 2; i <= 8; i++) {
+        add_pipe_part (pipe_offset, i * pipe_size, "block");
+    }
 }
 
 function clickHandler(event) {
@@ -75,18 +81,26 @@ function sHandler () {
  * This function updates the scene. It is called for every new frame.
  */
 function update() {
-
+game.physics.arcade.overlap(player, pipes, game_over);
 }
 
+function game_over() {
+    game.add.text(400, 200, "Game Over");
+    location.reload();
+}
 function moveleft() {
-    player.x -= 5
+    player.x -= 15
 }
 function moveright() {
-    player.x += 5
+    player.x += 15
 }
 function moveup() {
-    player.y -= 5
+    player.y -= 15
 }
 function movedown() {
-    player.y += 5
+    player.y += 15
+}
+
+function player_jump () {
+    player.body.velocity.y = -150;
 }
